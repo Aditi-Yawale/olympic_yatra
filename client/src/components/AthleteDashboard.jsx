2db6721ed85c6c8fc757278dc84bd9832776adb5
AukTech <aukshanilondhe@gmail.com>
2024-09-17 23:01:30 +0400

    added

---
+++ client/src/components/AthleteDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AthleteDashboard = () => {
+    return (
+        <div className="dashboard-container">
+            <h1>Athlete Dashboard</h1>
+            {/* Add content like Medal Tally, Performance Data, etc. */}
+        </div>
+    );
+};
+
+export default AthleteDashboard;
